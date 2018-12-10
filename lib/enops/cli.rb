64fb4837fb4af02f7e1e6c3196fef737dcd6fa10
Jason Weathered <jason@jasoncodes.com>
2018-12-10 11:59:21 +1000

    Extract CLI error handling and table rendering out of EB namespace

--- lib/enops/cli.rb
+++ lib/enops/cli.rb
@@ -1,10 +1,48 @@
 require 'clamp'
+require 'tty-table'
+require 'aws-sdk-core/errors'
 
 STDOUT.sync = true
 
 module Enops
   module CLI
     class MainCommand < Clamp::Command
+    end
+
+    module ErrorHandling
+      def execute
+        super
+      rescue Enops::UserMessageError, Enops::ExecuteError, Aws::Errors::ServiceError, Aws::Errors::MissingCredentialsError, Aws::Errors::InvalidProcessCredentialsPayload, Aws::Errors::MissingRegionError => e
+        $stderr.puts "#{e.message}"
+        exit 1
+      end
+    end
+
+    class Command < Clamp::Command
+      def self.inherited(subclass)
+        subclass.class_eval do
+          prepend ErrorHandling
+        end
+      end
+
+      def table(header: nil, rows:, key_labels: nil)
+        if header.nil?
+          keys = rows.map(&:keys).inject(:|)
+          header = keys.map { |key| key_labels&.fetch(key, nil) || key.to_s.titleize }
+          rows = rows.map { |row| row.values_at(*keys) }
+        else
+          raise ArgumentError if key_labels
+        end
+
+        table = TTY::Table.new header: header, rows: rows
+        puts table.render(width: 1e9) { |renderer|
+          renderer.border do
+            mid '='
+            mid_mid '  '
+            center '  '
+          end
+        }
+      end
     end
   end
 end
--- lib/enops/cli/elastic_beanstalk.rb
+++ lib/enops/cli/elastic_beanstalk.rb
@@ -1,46 +1,11 @@
-require 'tty-table'
 require 'active_support/inflector'
 
 module Enops::CLI::ElasticBeanstalk
-  module ErrorHandling
-    def execute
-      super
-    rescue Enops::UserMessageError, Enops::ExecuteError, Aws::Errors::ServiceError, Aws::Errors::MissingCredentialsError, Aws::Errors::InvalidProcessCredentialsPayload, Aws::Errors::MissingRegionError => e
-      $stderr.puts "#{e.message}"
-      exit 1
-    end
-  end
-
-  class Command < Clamp::Command
-    def self.inherited(subclass)
-      subclass.class_eval do
-        prepend ErrorHandling
-      end
-    end
-
+  class Command < Enops::CLI::Command
     private
 
     def api
       @api ||= Enops::ElasticBeanstalk.new
-    end
-
-    def table(header: nil, rows:, key_labels: nil)
-      if header.nil?
-        keys = rows.map(&:keys).inject(:|)
-        header = keys.map { |key| key_labels&.fetch(key, nil) || key.to_s.titleize }
-        rows = rows.map { |row| row.values_at(*keys) }
-      else
-        raise ArgumentError if key_labels
-      end
-
-      table = TTY::Table.new header: header, rows: rows
-      puts table.render(width: 1e9) { |renderer|
-        renderer.border do
-          mid '='
-          mid_mid '  '
-          center '  '
-        end
-      }
     end
   end
 
