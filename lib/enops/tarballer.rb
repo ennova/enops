e4d79e613a364d842dbf400e4ac14b887da42ed5
Jason Weathered <jason@jasoncodes.com>
2020-01-31 14:33:09 +1000

    Add helper class for creating small gzipped tarballs in memory

---
+++ lib/enops/tarballer.rb
@@ -0,0 +1,39 @@
+require 'enops/utils'
+require 'rubygems/package'
+require 'zlib'
+
+module Enops
+  class Tarballer
+    def initialize
+      reset
+    end
+
+    def reset
+      close
+      @io = StringIO.new
+      @io.binmode
+      @tar = Gem::Package::TarWriter.new(@io)
+    end
+
+    def close
+      @tar.close if @tar
+      @tar = nil
+    end
+
+    def gzipped_result
+      close
+      Zlib.gzip(@io.string)
+    end
+
+    def add_file(path, mode = nil, content = nil)
+      unless content
+        content ||= File.read(path, external_encoding: 'binary')
+        mode ||= File.stat(path).mode
+      end
+
+      @tar.add_file Utils.basepath(path), mode do |io|
+        io.write content
+      end
+    end
+  end
+end
