2c35e2e2c2cbffe2e99de7041bf774511ac87c3d
Jason Weathered <jason@jasoncodes.com>
2020-01-31 14:34:35 +1000

    Add class which can upload files and run a command on a remote host

---
+++ lib/enops/runner.rb
@@ -0,0 +1,142 @@
+require 'enops/utils'
+require 'enops/tarballer'
+require 'forwardable'
+require 'pty'
+require 'io/console'
+require 'base64'
+require 'shellwords'
+
+module Enops
+  class Runner
+    extend Forwardable
+
+    module Platform
+      class Local
+        def call(cmd)
+          cmd
+        end
+      end
+
+      class Remote
+        attr_reader :app_name
+
+        def initialize(app_name)
+          @app_name = app_name
+        end
+      end
+
+      class Heroku < Remote
+        def call(cmd)
+          "CI=true heroku run --app #{Shellwords.escape app_name} -- #{Shellwords.escape cmd}"
+        end
+      end
+
+      class ElasticBeanstalk
+        def call(cmd)
+          "enops eb run --app #{Shellwords.escape app_name} -- #{cmd}"
+        end
+      end
+    end
+
+    attr_accessor :platform
+    attr_accessor :work_dir
+    attr_accessor :extract_path
+    attr_accessor :command
+    attr_accessor :raise
+
+    def initialize
+      @platform = Platform::Local.new
+      @work_dir = nil
+      @extract_path = nil
+      @command = 'bash -i'
+      @raise = false
+    end
+
+    def_delegators :tarballer, :add_file
+
+    def execute
+      input_thread = nil
+
+      PTY.spawn(spawn_cmd) do |read, write, pid|
+        Signal.trap(:WINCH) { write.winsize = STDOUT.winsize }
+        input_thread = Thread.new { IO.copy_stream(STDIN, write) }
+
+        begin
+          IO.console&.raw!
+
+          quiet = false
+          buf = ''
+
+          loop do
+            char = begin
+              read.readchar
+            rescue EOFError, Errno::EIO
+              break
+            end
+
+            unless quiet
+              STDOUT.print char
+            end
+
+            if buf
+              buf.concat char
+              if buf =~ /(?<! )enops-upload$/
+                STDOUT.write "\33[2K\r"
+                quiet = true
+                buf = ''
+                Thread.new { write.write bootstrap_data }
+              end
+              if buf =~ /(?<! )enops-exec$/
+                quiet = false
+                buf = nil
+              end
+            end
+          end
+
+          Process.wait(pid)
+        ensure
+          IO.console&.cooked!
+        end
+
+        unless $?.success?
+          if raise
+            raise ExecuteError.new(cmd: cmd, status: $?, output: nil)
+          else
+            exit $?.exitstatus || 130
+          end
+        end
+      end
+
+      input_thread.kill if input_thread
+    end
+
+    private
+
+    def tarballer
+      @tarballer ||= Tarballer.new
+    end
+
+    def bootstrap_data
+      Base64.encode64(tarballer.gzipped_result)
+    end
+
+    def bootstrap_script
+      <<~SH.tr_s("\n", ';')
+        set -euo pipefail
+        echo -en enops\\\\x2dupload
+        dd bs=1 count=#{bootstrap_data.bytesize} | base64 --decode | tar zx #{"-C #{Shellwords.escape extract_path}" if extract_path}
+        echo -en enops\\\\x2dexec
+        #{"cd #{Shellwords.escape(work_dir).sub(/\A\\~/, '~')}" if work_dir}
+        exec #{command}
+      SH
+    end
+
+    def bootstrap_cmd
+      "bash -c #{Shellwords.escape bootstrap_script}"
+    end
+
+    def spawn_cmd
+      platform.call(bootstrap_cmd)
+    end
+  end
+end
